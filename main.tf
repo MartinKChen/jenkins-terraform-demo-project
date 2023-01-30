0ed14b3e5241dfe5d352a0af3db1dc1b0c3358c9
Martin Chen <54146987+MartinKChen@users.noreply.github.com>
2023-01-30 10:12:38 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2_private" {
+  ami = "ami-0e2bf1ada70fd3f33"
+  instance_type = "t2.micro"
+  key_name = "ssh-key"
+  security_groups = ["sg-096b5e089c243bf8a"]
+  subnet_id = "subnet-0b6658a9d6727bb31"
+  associate_public_ip_address = false
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    "Name" = "TERRAFORM"
+  }
+}
